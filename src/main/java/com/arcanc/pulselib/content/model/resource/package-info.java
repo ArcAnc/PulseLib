2b0e2ee9cf1f43235781cb353eaa802fd1802f3d
ArcAnc <ArcAnc13@gmail.com>
2026-09-23 13:46:31 +0300

    fixed headers

--- src/main/java/com/arcanc/pulselib/content/model/resource/package-info.java
+++ src/main/java/com/arcanc/pulselib/content/model/resource/package-info.java
@@ -1,2 +1,13 @@
-@org.jspecify.annotations.NullMarked
+/**
+ * @author ArcAnc
+ * Created at: 23.09.2026
+ * Copyright (c) 2026
+ * <p>
+ * This code is licensed under "Arc's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
+@NullMarked
 package com.arcanc.pulselib.content.model.resource;
+
+import org.jspecify.annotations.NullMarked;
--- src/main/java/com/arcanc/pulselib/util/PResourceCache.java
+++ src/main/java/com/arcanc/pulselib/util/PResourceCache.java
@@ -1,3 +1,12 @@
+/**
+ * @author ArcAnc
+ * Created at: 23.09.2026
+ * Copyright (c) 2026
+ * <p>
+ * This code is licensed under "Arc's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
 package com.arcanc.pulselib.util;
 
 import com.arcanc.pulselib.content.event.PulseLibEvents;
